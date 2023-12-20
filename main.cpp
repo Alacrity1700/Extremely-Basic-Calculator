612e3f71f6f4829630f6566f403fedb329653c36
Ivan <91813660+Alacrity1700@users.noreply.github.com>
2023-12-20 11:18:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,54 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 float inputValue() {
     float value{};
-    std::cout << "Input a value:\n";
-    std::cin >> value;
+    cout << "Input a value:\n";
+    cin >> value;
     return value;
 }
 
-void calcAnswer(int Operator, float firstVal, float secVal, float& finalVal) {
-    if (Operator == 1) {
-        finalVal = firstVal * secVal;
-    }
-    else if (Operator == 2) {
-        finalVal = firstVal / secVal;
-    }
-    else if (Operator == 3) {
-        finalVal = firstVal + secVal;
-    }
-    else if (Operator == 4) {
-        finalVal = firstVal - secVal;
-    }
-}
-
 int main() {
-    SetConsoleTitle(L"Ivan's Calculator");
+    SetConsoleTitle(L"Calculator");
 
     bool exitProgram = false;
     float finalVal;
 
     do {
         float firstVal = inputValue();
-        std::cout << "\nChoose one (1,2,3,4):\n1. Multiply\n2. Divide\n3. Add\n4. Subtract\n" << std::endl;
-        int Operator;
-        std::cin >> Operator;
+        cout << "\nChoose an operator (1,2,3,4):\n1. Multiply\n2. Divide\n3. Add\n4. Subtract\n" << endl;
+        int Operator{};
+        cin >> Operator;
+      
         float secVal = inputValue();
 
+        switch (Operator) {
+        case 1:
+            finalVal = firstVal * secVal;
+            break;
+
+        case 2:
+            finalVal = firstVal / secVal;
+            break;
+
+        case 3:
+            finalVal = firstVal + secVal;
+            break;
+
+        case 4:
+            finalVal = firstVal - secVal;
+            break;
+        default:
+            cout << "Enter a valid operator. (1-4)\n";
+            return Operator;
+        }
         
-        calcAnswer(Operator, firstVal, secVal, finalVal);
-        std::cout << "Solution: " << finalVal << std::endl;
-
-      
-        std::cout << "Do you want to perform another calculation? (1 for Yes, 0 for No): ";
-        std::cin >> exitProgram;
-
+        cout << "Solution: " << finalVal << endl;
+    
+        cout << "Do you want to perform another calculation? (1 for Yes, 0 for No): ";
+        cin >> exitProgram;
+       
     } while (exitProgram);
 
     return 0;
